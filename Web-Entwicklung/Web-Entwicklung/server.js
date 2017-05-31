b5dc8ba3e0d725816006f7dbeaf134f338a55d9d
Enyaa <arnolds.lena@yahoo.com>
2017-05-31 10:29:41 +0200

    ESLint, map in index, css

--- Web-Entwicklung/Web-Entwicklung/server.js
+++ Web-Entwicklung/Web-Entwicklung/server.js
@@ -1,19 +1,18 @@
-var express = require('express')
-    , app = express()
-    , server = require('http').createServer(app)
-    , io = require('socket.io').listen(server)
-    , conf = require('./config.json');
+var express = require("express");
+var app = express();
+var server = require("http").createServer(app);
+var io = require("socket.io").listen(server);
+var conf = require("./config.json");
 
 server.listen(conf.port);
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 // wenn der Pfad / aufgerufen wird
-app.get('/', function (req, res) {
-    // so wird die Datei index.html ausgegeben
-    res.sendfile(__dirname + '/public/index.html');
+app.get("/", function (req, res) {
+    //so wird die Datei index.html ausgegeben
+    res.sendfile(__dirname + "/public/index.html");
 });
 
-
 // Portnummer in die Konsole schreiben
-console.log('Der Server läuft nun unter http://127.0.0.1:' + conf.port + '/');
+console.log("Der Server laeuft nun unter http://127.0.0.1:" + conf.port + "/");
