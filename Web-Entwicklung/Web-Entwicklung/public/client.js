0bb5d88bcb5daf0961f49e42cdaf0ee2bf06ce91
Enyaa <arnolds.lena@yahoo.com>
2017-06-19 15:53:56 +0200

    index viewport meta tag, css flex, resize test

--- Web-Entwicklung/Web-Entwicklung/public/client.js
+++ Web-Entwicklung/Web-Entwicklung/public/client.js
@@ -16,3 +16,21 @@ GoogleMapsLoader.load(function (google) {
 	//Map erstellen und in div Element einfügen
 	map = new google.maps.Map(document.getElementById("map"), mapOptions);
 });
+
+/*
+GoogleMapsLoader.event.addDomListener(window, "resize", function () {
+	var center = map.getCenter();
+	GoogleMapsLoader.maps.event.trigger(map, "resize");
+	map.setCenter(center);
+	window.alert("Resize triggered!");
+});
+
+GoogleMapsLoader.onLoad(function (google) {
+	window.alert("onload");
+	google.event.addDomListener(window, "resize", function () {
+		var center = map.getCenter();
+		google.maps.event.trigger(map, "resize");
+		map.setCenter(center);
+		window.alert("Resize triggered!");
+	});
+});*/
