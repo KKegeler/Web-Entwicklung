c202520d018dce5fff88026ae9268ace091b9510
Enyaa <arnolds.lena@yahoo.com>
2017-07-23 17:51:32 +0200

    höhenprofil stuff

--- Web-Entwicklung/Web-Entwicklung/public/client.js
+++ Web-Entwicklung/Web-Entwicklung/public/client.js
@@ -5,7 +5,6 @@ var eintraegeProSeite;
 var list = document.getElementById("list");
 var GoogleMapsLoader = require("google-maps");
 var paginationdiv = document.getElementById("pagination");
-var paginaton = require("pagination");
 var url = document.URL;
 //API-Key setzen
 GoogleMapsLoader.KEY = "AIzaSyAqOM-iRIWZHE6f5x0wUF7fAFvCPuyKAFY";
@@ -62,7 +61,7 @@ fetch(url + "tracklist").then(response => {
 	fuelleListe(result);
 }).catch(error => {
 	console.error(error.message);
-	});
+});
 
 function paginate() {
 	let browserhöhe = document.documentElement.clientHeight;
@@ -82,32 +81,29 @@ function paginate() {
 		let id = children[v].getAttribute("id");
 		let elem = document.getElementById(id);
 		if (v > eintraegeProSeite) {
-			elem.style.display = 'none';
-			//elemente ausblenden
-		} else {
-			elem.style.display = 'block';
+			elem.style.display = "none";
+		} //elemente ausblenden
+		else {
+			elem.style.display = "block";
 		}
 	}
 
 	paginationdiv.onclick = function (event) {
 		let seitenid = event.target.getAttribute("class");
 		let vorherigeSeite = seitenid - 1;
-		let von = eintraegeProSeite * vorherigeSeite + 1;
+		let von = (eintraegeProSeite * vorherigeSeite) + 1;
 		let bis = seitenid * eintraegeProSeite;
 		let childs = list.childNodes;
-		for (let i = 1; i< childs.length; i++) {
+		for (let i = 1; i < childs.length; i++) {
 			let Id = childs[i].getAttribute("id");
-			document.getElementById(Id).style.display = 'none';
+			document.getElementById(Id).style.display = "none";
 		}
 
 		for (let i = von; i < bis; i++) {
 			let id = childs[i].getAttribute("id");
-			document.getElementById(id).style.display = 'block';
-			
+			document.getElementById(id).style.display = "block";
 		}
-
-	}
-
+	};
 }
 
 function eintraegeProSeiteBerechnen() {
@@ -131,7 +127,6 @@ function fuelleListe(obj) {
 	//OnClick wird an die Liste angehangen,client stellt wieder anfrage nach dem speziellen track
 	list.onclick = function (event) {
 		var geklickteId = event.target.getAttribute("id");
-		
 		fetch(url + "tracklist/" + geklickteId).then(response => {
 			if (response.ok) {
 				return response.json();
@@ -141,10 +136,10 @@ function fuelleListe(obj) {
 			}
 		}).then(result => {
 			makeCoordinaten(result);
+			drawHeightProfile(result);
 		}).catch(error => {
 			console.error(error.message);
-			});
-		
+		});
 	};
 }
 //Client bekommt Koordinaten des Tracks zurück und setzt den Pfad der Polyline und die Grenzen
@@ -162,4 +157,77 @@ function makeCoordinaten(coords) {
 		polyline.setMap(map);
 		map.fitBounds(bounds);
 	});
+}
+
+var myCanvas = createNode("canvas");
+myCanvas.width = 400;
+myCanvas.height = 200;
+var myCanvasDiv = document.getElementById("heightProfile")
+var ctx = myCanvas.getContext("2d");
+append(myCanvasDiv, myCanvas);
+
+var highestGlobal = 0;
+getHighestGlobal();
+
+function getHighest(coords) {
+	var vals = coords;
+	var tmp = vals[0][2];
+	for (var i = 1; i < vals.length; i++) {
+		if (tmp < vals[i][2]) {
+			tmp = vals[i][2];
+		}
+	}
+	if (highestGlobal < tmp) {
+		highestGlobal = tmp;
+	}
+}
+
+function getHighestGlobal() {
+	for (var i = 1; i < 66; i++) {
+		fetch(url + "tracklist/" + i).then(response => {
+			if (response.ok) {
+				return response.json();
+			}
+			else {
+				return null;
+			}
+		}).then(result => {
+			getHighest(result);
+		}).catch(error => {
+			console.error(error.message);
+		});
+	}
+}
+
+function drawHeightProfile(coords) {
+	var coordinates = coords;
+	var heightValues = [];
+	var points = coordinates.length;
+	var val = 1.00;
+	val /= points;
+	val *= 400;
+	val = Number(val).toFixed(2);
+	console.log("Val: " + val);
+
+	for (let i = 0; i < points; i++) {
+		heightValues.push(coordinates[i][2]);
+	}
+	ctx.clearRect(0, 0, 400, 200);
+	ctx.fillStyle = "rgba(0, 0, 0, 0.5)";
+	ctx.fillRect(0, 0, 400, 200);
+	ctx.beginPath();
+	ctx.moveTo(0, 200 - ((heightValues[0] / highestGlobal) * 200));
+	var width = parseFloat(val);
+	var height;
+	for (var j = 1; j < points; j++) {
+		height = (heightValues[j] / highestGlobal) * 200;
+		height = 200 - height;
+		ctx.lineTo(width, height);
+		width = Number(parseFloat(parseFloat(width) + parseFloat(val))).toFixed(2);
+	}
+	console.log("Temp: " + width);
+	ctx.strokeStyle = "#FFFFFF";
+	ctx.strokeWidth = 20;
+	ctx.stroke();
+	console.log("Höhenprofil: " + heightValues[0]);
 }
