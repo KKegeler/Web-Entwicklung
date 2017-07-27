2498ec038d789f80d914dc42561d7c93d39a9d47
Kai Kegeler <kai.kegeler@outlook.de>
2017-07-27 13:57:01 +0200

    Modularisierung angefangen/versucht

---
+++ Web-Entwicklung/Web-Entwicklung/Javascript/GoogleMap.js
@@ -0,0 +1,71 @@
+﻿//npm modul google-maps einbinden (Wrapper für Google Maps API)
+var GoogleMapsLoader = require("google-maps");
+var pagination = require("./pagination");
+var heightProfile = require("./HeightProfile");
+var map;
+var polyline;
+var url = document.URL;
+var client = require("./client");
+//API-Key setzen
+GoogleMapsLoader.KEY = "AIzaSyAqOM-iRIWZHE6f5x0wUF7fAFvCPuyKAFY";
+
+//Google Maps API laden
+GoogleMapsLoader.load(function (google) {
+	//Map-Options setzen
+	var mapOptions = {
+		//Längengrad und Breitengrad von Trier angeben
+		center: new google.maps.LatLng(49.751185, 6.636405),
+		//Zoom setzen
+		zoom: 13
+	};
+	//Map erstellen und in div Element einfügen
+	map = new google.maps.Map(document.getElementById("map"), mapOptions);
+	polyline = new google.maps.Polyline({ strokeColor: "#FF0000", strokeOpacity: 1.0, strokeWeight: 2 });
+});
+
+//Client bekommt Trackliste und erstellt die Liste
+function fillList(obj) {
+	for (var i = 0; i < obj.names.length; i++) {
+		let li = client.createNode("li");
+		li.innerHTML = obj.names[i];
+		li.setAttribute("ID", "" + obj.ids[i]);
+		client.append(list, li);
+	}
+	pagination.calculateEntriesPerPage();
+
+	//OnClick wird an die Liste angehangen,client stellt wieder anfrage nach dem speziellen track
+	list.onclick = function (event) {
+		var clickedId = event.target.getAttribute("id");
+		heightProfile.latestId = clickedId;
+		fetch(url + "tracklist/" + clickedId).then(response => {
+			if (response.ok) {
+				return response.json();
+			}
+			else {
+				return null;
+			}
+		}).then(result => {
+			makeCoordinates(result);
+			heightProfile.drawHeightProfile(result);
+		}).catch(error => {
+			console.error(error.message);
+		});
+	};
+}
+//Client bekommt Koordinaten des Tracks zurück und setzt den Pfad der Polyline und die Grenzen
+function makeCoordinates(coords) {
+	var path = [];
+	var coordinates = coords;
+	GoogleMapsLoader.load(function (google) {
+		var bounds = new google.maps.LatLngBounds();
+		for (let j = 0; j < coordinates.length; j++) {
+			path.push(new google.maps.LatLng(coordinates[j][0], coordinates[j][1]));
+			bounds.extend(new google.maps.LatLng(coordinates[j][0], coordinates[j][1]));
+		}
+		polyline.setPath(path);
+		polyline.setMap(map);
+		map.fitBounds(bounds);
+	});
+}
+
+module.exports = fillList();
---
+++ Web-Entwicklung/Web-Entwicklung/Javascript/pagination.js
@@ -0,0 +1,89 @@
+﻿var previous = document.getElementById("previousPage");
+var next = document.getElementById("nextPage");
+var current = document.getElementById("currentPage");
+var pages = document.getElementById("Pages");
+var entriesPerPage;
+
+function paginate() {
+	//benötigte Werte holen
+	let currentPage = parseInt(current.textContent);
+	let children = list.childNodes;
+	let currentPages = Math.floor(children.length / entriesPerPage) + 1;
+
+	if (currentPages === 67) {
+		currentPages = 65;
+	}
+
+	pages.innerHTML = currentPages;
+
+	//Nicht benötigte Einträge unsichtbar machen
+	for (let v = 1; v < children.length; v++) {
+		let id = children[v].getAttribute("id");
+		let elem = document.getElementById(id);
+		if (v > entriesPerPage) {
+			elem.style.display = "none";
+			//elemente ausblenden
+		}
+		else {
+			elem.style.display = "block";
+		}
+	}
+
+	//OnClick Listener von previous
+	previous.onclick = function () {
+		currentPage = parseInt(current.textContent);
+		if (currentPage === 1) {
+			current.innerHTML = currentPage;
+		}
+		else {
+			let newPage = currentPage - 1;
+			current.innerHTML = newPage;
+			togglePages(newPage);
+		}
+	};
+
+	//OnClick Listener von Next
+	next.onclick = function () {
+		let pagesValue = parseInt(pages.textContent);
+		currentPage = parseInt(current.textContent);
+		if (currentPage === pagesValue) {
+			//console.log("Next OnClick if");
+			current.innerText = currentPage;
+		}
+		else {
+			//console.log("Next OnClick else");
+			let newCurrentPage = currentPage + 1;
+			current.innerText = newCurrentPage;
+			togglePages(newCurrentPage);
+		}
+	};
+}
+
+function togglePages(currentPage) {
+	let previousPage = currentPage - 1;
+	let from = (entriesPerPage * previousPage) + 1;
+	let to = currentPage * entriesPerPage;
+	let children = list.childNodes;
+
+	for (let i = 1; i < children.length; i++) {
+		let id = children[i].getAttribute("id");
+		document.getElementById(id).style.display = "none";
+	}
+
+	for (let i = from; i <= to; i++) {
+		let id = children[i].getAttribute("id");
+		document.getElementById(id).style.display = "block";
+	}
+}
+
+//Die Einträge pro Seite berechnen,wird am Anfang aufgerufen und bei jedem Reseize
+function calculateEntriesPerPage() {
+	let browserHeight = window.innerHeight;
+	//console.log(browserHeight);
+	//Berechnung durch Höhe des Fensters - Höhe der Pagination div geteilt durch Höhe pro li-Element
+	let newEntries = Math.round((browserHeight - 22) / 32);
+	entriesPerPage = newEntries;
+	paginate();
+}
+
+module.exports = calculateEntriesPerPage();
